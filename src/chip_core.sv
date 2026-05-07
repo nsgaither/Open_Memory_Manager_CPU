ac6769e27cef49ef36f52df2610b8412c821f113
Jack Wong <jac.wong.k@gmail.com>
2026-05-07 15:21:23 -0700

    modules instantiated and connected still need to configure pins

--- src/chip_core.sv
+++ src/chip_core.sv
@@ -31,58 +31,51 @@ module chip_core #(
 
     inout  wire [NUM_ANALOG_PADS-1:0] analog  // Analog
 );
+
+    // Serial Interface
+    localparam NUM_TPINS = 9;
+    localparam NUM_RPINS = 9;
     
+    // Cache TODO: no idea what these are yet
+    localparam NUM_SETS = ;
+    localparam WORDS_PER_LINE = ;
+
+    wire clk_i, rst_ni;
+    assign clk_i = clk;
+    assign rst_ni = rst_n;
+
     // PicoRV32 memory interface
-    logic        mem_valid;
-    logic        mem_instr;
-    logic        mem_ready;
+    wire        mem_valid;
+    wire        mem_instr;
+    wire        mem_ready;
     
-    logic [31:0] mem_addr;
-    logic [31:0] mem_wdata;
-    logic [3:0]  mem_wstrb;
-    logic [31:0] mem_rdata;
+    wire [31:0] mem_addr;
+    wire [31:0] mem_wdata;
+    wire [3:0]  mem_wstrb;
+    wire [31:0] mem_rdata;
     
     // Lookahead interface (can be left unused)
-    logic        mem_la_read;
-    logic        mem_la_write;
-    logic [31:0] mem_la_addr;
-    logic [31:0] mem_la_wdata;
-    logic [3:0]  mem_la_wstrb;
+    wire        mem_la_read;
+    wire        mem_la_write;
+    wire [31:0] mem_la_addr;
+    wire [31:0] mem_la_wdata;
+    wire [3:0]  mem_la_wstrb;
     
     // Trace/debug (can leave unconnected if unused)
-    logic        trace_valid;
-    logic [35:0] trace_data;
-
+    wire        trace_valid;
+    wire [35:0] trace_data;
+z
     // PCPI interface (tied off - not using external coprocessor)
-    logic        pcpi_valid;
-    logic [31:0] pcpi_insn;
-    logic [31:0] pcpi_rs1;
-    logic [31:0] pcpi_rs2;
-    logic        pcpi_wr;
-    logic [31:0] pcpi_rd;
-    logic        pcpi_wait;
-    logic        pcpi_ready;
+    wire        pcpi_valid;
+    wire [31:0] pcpi_insn;
+    wire [31:0] pcpi_rs1;
+    wire [31:0] pcpi_rs2;
+    wire        pcpi_wr;
+    wire [31:0] pcpi_rd;
+    wire        pcpi_wait;
+    wire        pcpi_ready;
 
-    logic        trap;
-
-    (* keep *) mem_ctrl_128x32 #(
-    ) mem_ctrl (
-        .clk_i       (clk),
-        .rst_ni      (rst_n),
-        .mem_valid_i (mem_valid),
-        .mem_instr_i (mem_instr),
-        .mem_addr_i  (mem_addr),
-        .mem_wdata_i (mem_wdata),
-        .mem_wstrb_i (mem_wstrb),
-        .mem_rdata_o (mem_rdata),
-        .mem_ready_o (mem_ready)
-		`ifdef USE_POWER_PINS
-		// verilator lint_off ASSIGNIN
-		,.VDD(VDD)
-		,.VSS(VSS)
-		// verilator lint_on ASSIGNIN
-		`endif
-    );
+    wire        trap;
 
 
     picorv32 #(
@@ -121,8 +114,8 @@ module chip_core #(
         .PROGADDR_IRQ         (32'h0000_0010),
         .STACKADDR            (32'h0000_2000)
     ) pico_rv32_cpu (
-        .clk         (clk),
-        .resetn      (rst_n),
+        .clk         (clk_i),
+        .resetn      (rst_ni),
 
         .trap        (trap),
 
@@ -161,6 +154,201 @@ module chip_core #(
         .trace_valid (trace_valid),
         .trace_data  (trace_data)
     );
+
+    // special address handler
+    // core -> this -> cache controller
+    // outputs
+    wire pass_mem_valid;
+    wire pass_mem_ready;
+    wire [31:0] pass_mem_addr;
+    wire [31:0] pass_mem_wdata;
+    wire [3:0] pass_mem_wstrb;
+    wire [31:0] pass_mem_rdata;
+
+    wire flush_ready;
+    wire [31:0] flush_addr;
+    wire flush_valid;
+
+    wire gpio_pins_o;
+    wire gpio_pins_i;
+    wire gpio_dir;
+
+    wire cpu_id;
+
+    sp_addr_handler u_sp_addr_handler (
+    .clk_i           (clk_i),
+    .rst_ni          (rst_ni),
+
+    // Interface from CPU (native picorv32)
+    .mem_valid       (mem_valid),
+    .mem_ready       (mem_ready),
+    .mem_addr        (mem_addr),
+    .mem_wdata       (mem_wdata),
+    .mem_wstrb       (mem_wstrb),
+    .mem_rdata       (mem_rdata),
+
+    // Downstream passthrough interface
+    .pass_mem_valid  (pass_mem_valid),
+    .pass_mem_ready  (pass_mem_ready),
+    .pass_mem_addr   (pass_mem_addr),
+    .pass_mem_wdata  (pass_mem_wdata),
+    .pass_mem_wstrb  (pass_mem_wstrb),
+    .pass_mem_rdata  (pass_mem_rdata),
+
+    // Flush special instruction
+    .flush_ready_i   (flush_ready),
+    .flush_addr_o    (flush_addr),
+    .flush_valid_o   (flush_valid),
+
+    // GPIO pin connections
+    .gpio_pins_o     (gpio_pins_o),
+    .gpio_pins_i     (gpio_pins_i),
+    .gpio_dir_o      (gpio_dir),
+
+    // CPU ID
+    .cpu_id_i        (cpu_id)
+);
+
+// Cache Controller
+// sp addr handler -> this -> cache interposer
+//                     |
+//                     v
+//                   Cache
+wire        cache_valid;
+wire [31:0] cache_addr;
+wire [31:0] cache_data;
+wire [ 8:0] cache_cmd;
+wire        cache_ready;
+
+wire        bus_valid;
+wire [31:0] bus_data;
+wire [ 2:0] bus_dircmd;
+wire        bus_ready;
+
+wire        snoop_valid;
+wire [31:0] snoop_data;
+wire [ 2:0] snoop_dircmd;
+wire        snoop_ready;
+
+cache_controller #(
+  .NumSets     (NUM_SETS),
+  .WordsPerLine (WORDS_PER_LINE)
+) i_cache_controller (
+  .clk_i                 (clk_i),
+  .rst_ni                (rst_ni),
+
+  .mem_valid_i           (pass_mem_valid),
+  .mem_ready_o           (pass_mem_ready),
+  .mem_addr_i            (pass_mem_addr),
+  .mem_wdata_i           (pass_mem_wdata),
+  .mem_wstrb_i           (pass_mem_wstrb),
+  .mem_rdata_o           (pass_mem_rdata),
+
+  .flush_valid_i         (flush_valid),
+  .flush_addr_i          (flush_addr),
+  .flush_ready_o         (flush_ready),
+
+  .data_cache_rd_en_o    (),
+  .data_cache_rd_set_o   (),
+  .data_cache_rd_word_o  (),
+  .data_cache_rd_data_i  (),
+
+  .data_cache_wr_en_o    (),
+  .data_cache_wr_set_o   (),
+  .data_cache_wr_word_o  (),
+  .data_cache_wr_data_o  (),
+  .data_cache_wr_strb_o  (),
+  .data_cache_ready_i    (),
+
+  .cache_valid_o         (cache_valid),
+  .cache_ready_i         (cache_ready),
+  .cache_addr_o          (cache_addr),
+  .cache_data_o          (cache_data),
+  .cache_cmd_o           (cache_cmd),
+
+  .bus_valid_i           (bus_valid),
+  .bus_ready_o           (bus_ready),
+  .bus_data_i            (bus_data),
+  .bus_dircmd_i          (bus_dircmd),
+
+  .snoop_valid_i         (snoop_valid),
+  .snoop_ready_o         (snoop_ready),
+  .snoop_data_i          (snoop_data),
+  .snoop_dircmd_i        (snoop_dircmd)
+);
+
+// Cache
+// Cache controller -> this
+cache #(
+  .NumSets     (NUM_SETS),
+  .WordsPerLine (WORDS_PER_LINE)
+) i_cache (
+  .clk_i     (clk_i),
+  .rst_ni    (rst_ni),
+
+  .rd_en_i   (),
+  .rd_set_i  (),
+  .rd_word_i (),
+  .rd_data_o (),
+
+  .wr_en_i   (),
+  .wr_set_i  (),
+  .wr_word_i (),
+  .wr_data_i (),
+  .wr_strb_i (),
+
+  .ready_o   ()
+);
+
+// cache_interposer_interface
+// cache controller -> this -> IO pins
+wire                 rbusy;
+
+wire                 req_o;
+wire [NUM_TPINS-1:0] serial_o;
+wire                 req_i;
+wire [NUM_RPINS-1:0] serial_i;
+
+cache_interface #(
+    .NUM_TPINS (NUM_TPINS),
+    .NUM_RPINS (NUM_RPINS)
+) u_cache_interface (
+    .clk_i          (clk_i),
+    .rst_ni         (rst_ni),
+
+    // UPSTREAM --------------------------------------
+    // Cache Send Ports
+    .cache_valid_i  (cache_valid),
+    .cache_addr_i   (cache_addr),
+    .cache_data_i   (cache_data),
+    .cache_cmd_i    (cache_cmd),
+    .cache_ready_o  (cache_ready),
+
+    // Bus Ack ports
+    .bus_valid_o    (bus_valid),
+    .bus_data_o     (bus_data),
+    .bus_dircmd_o   (bus_dircmd),
+    .bus_ready_i    (bus_ready),
+
+    // Snoop Req ports
+    .snoop_valid_o  (snoop_valid),
+    .snoop_data_o   (snoop_data),
+    .snoop_dircmd_o (snoop_dircmd),
+    .snoop_ready_i  (snoop_ready),
+
+    // busy
+    .rbusy_o        (rbusy),
+
+    // other
+    .cpu_id_o       (cpu_id),
+
+    // DOWNSTREAM ------------------------------------
+    // wrapped serializer IO
+    .req_i          (req_i),
+    .serial_i       (serial_i),
+    .req_o          (req_o),
+    .serial_o       (serial_o)
+);
 
     
     // See here for usage: https://gf180mcu-pdk.readthedocs.io/en/latest/IPs/IO/gf180mcu_fd_io/digital.html
