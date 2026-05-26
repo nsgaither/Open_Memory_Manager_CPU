c400038c0158f83d7c7ca5598764b30f1946d764
Nicholas Gaither <nicholas.gaither@gmail.com>
2026-05-25 21:46:02 -0700

    remove confusion, make sure only 0p5x0p5 available.

--- src/slot_defines.svh
+++ src/slot_defines.svh
@@ -1,36 +1,3 @@
-`ifdef SLOT_1X1
-
-// Power/ground pads for core and I/O
-`define NUM_DVDD_PADS 8
-`define NUM_DVSS_PADS 10
-
-// Signal pads
-`define NUM_BIDIR_PADS 40
-
-`endif
-
-`ifdef SLOT_0P5X1
-
-// Power/ground pads for core and I/O
-`define NUM_DVDD_PADS 8
-`define NUM_DVSS_PADS 8
-
-// Signal pads
-`define NUM_BIDIR_PADS 44
-
-`endif
-
-`ifdef SLOT_1X0P5
-
-// Power/ground pads for core and I/O
-`define NUM_DVDD_PADS 8
-`define NUM_DVSS_PADS 8
-
-// Signal pads
-`define NUM_BIDIR_PADS 46
-
-`endif
-
 `ifdef SLOT_0P5X0P5
 
 // Power/ground pads for core and I/O
